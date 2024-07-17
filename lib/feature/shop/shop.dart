b2dff5179dd75723bf894c903a8190f67d8c1b2d
SingHuyLeang <leang6525@gmail.com>
2024-07-17 11:31:50 +0700

    Delete product v2

--- lib/feature/shop/shop.dart
+++ lib/feature/shop/shop.dart
@@ -1,12 +1,8 @@
-import 'dart:developer';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_11_12/feature/shop/model/product_model.dart';
+import 'package:firebase_11_12/widget/card_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-import 'controller/shop_controller.dart';
 
 // ignore: must_be_immutable
 class ShopScreen extends StatelessWidget {
@@ -126,74 +122,6 @@ class ShopScreen extends StatelessWidget {
             );
           }
         },
-      ),
-    );
-  }
-}
-
-class CardItem extends StatelessWidget {
-  CardItem({super.key, required this.product, required this.docId});
-
-  final ProductModel product;
-  final String docId;
-
-  final controller = Get.put(ShopController());
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onLongPress: () {
-        Get.bottomSheet(
-          Container(
-            width: double.infinity,
-            height: 100,
-            color: Colors.white,
-            child: Column(
-              children: [
-                TextButton(
-                  onPressed: () {},
-                  child: Text('Update',
-                      style: Theme.of(context).textTheme.bodyMedium),
-                ),
-                TextButton(
-                  onPressed: () async => controller.deleteProduct(docId),
-                  child: Text(
-                    'Delete',
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyMedium!
-                        .apply(color: Colors.red),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image(
-              image: NetworkImage(product.image),
-            ),
-            Text(
-              product.title,
-              style: Theme.of(context).textTheme.bodyMedium!.apply(
-                    color: Colors.grey[700],
-                  ),
-            ),
-            Text(
-              '\$ ${product.price}',
-              style: Theme.of(context).textTheme.bodyMedium!.apply(
-                    color: Colors.black,
-                    fontWeightDelta: DateTime.may,
-                  ),
-            ),
-          ],
-        ),
       ),
     );
   }
---
+++ lib/widget/card_item.dart
@@ -0,0 +1,73 @@
+
+import 'package:firebase_11_12/feature/shop/controller/shop_controller.dart';
+import 'package:firebase_11_12/feature/shop/model/product_model.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CardItem extends StatelessWidget {
+  CardItem({super.key, required this.product, required this.docId});
+
+  final ProductModel product;
+  final String docId;
+
+  final controller = Get.put(ShopController());
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onLongPress: () {
+        Get.bottomSheet(
+          Container(
+            width: double.infinity,
+            height: 100,
+            color: Colors.white,
+            child: Column(
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text('Update',
+                      style: Theme.of(context).textTheme.bodyMedium),
+                ),
+                TextButton(
+                  onPressed: () async => controller.deleteProduct(docId),
+                  child: Text(
+                    'Delete',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium!
+                        .apply(color: Colors.red),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(
+              image: NetworkImage(product.image),
+            ),
+            Text(
+              product.title,
+              style: Theme.of(context).textTheme.bodyMedium!.apply(
+                    color: Colors.grey[700],
+                  ),
+            ),
+            Text(
+              '\$ ${product.price}',
+              style: Theme.of(context).textTheme.bodyMedium!.apply(
+                    color: Colors.black,
+                    fontWeightDelta: DateTime.may,
+                  ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
